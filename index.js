581c95ee3b0412a37e75b454c507469a5b235f66
unknown <habiburrahman5236@gmail.com>
2025-12-04 22:12:56 +0600

    js array fuction

---
+++ index.js
@@ -0,0 +1,58 @@
+// function add(b, c) {
+//     let a = b + c;
+//     return a
+
+
+// }
+// let b = add(5, 6)
+// console.log(b)
+
+// let num = Number(prompt("enter your number:"));
+// for (let i = 1; i <= num; i++) {
+//     console.log(i)
+// }
+
+// let latter = 'abc';
+// let latter2 = 'def'
+// console.log(latter2 + ' ' + latter);
+
+// function sum(num1, num2) {
+//     let result = num1 + num2;
+//     let final = result * 5;
+//     return final;
+// }
+// let output = sum(10, 12)
+// console.log(output)
+
+
+// if (output > 20) {
+//     console.log('good')
+//     let output2 = sum(40, 20)
+//     console.log(output2)
+// }
+// else {
+//     console.log('bad')
+// }
+
+let names = ['habib', 'labib', 'shabib', 'nabib'];
+// names.push('nobab')
+// console.log(names)
+// names.unshift('dobab')
+// console.log(names)
+// names.pop()
+// console.log(names)
+// names.shift()
+// console.log(names)
+// names.reverse()
+// names.sort()
+// console.log(names)
+
+// for (let i = 0; i <= names.length; i++) {
+//     console.log(names[i], i)
+// }
+
+// const numbers = [5, 8, 9];
+// console.log(numbers[2])
+// console.log(names[2])
+let doubleArray = [[1, 2, 3], [4, 5, 6]];
+console.log(doubleArray[0][1])
